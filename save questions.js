95928c60c5cb6bbc774b87c891754f91db5f7feb
Ahmed Hosny <ahhosnyas@gmail.com>
2022-12-30 23:55:00 +0200

    minor update

--- save questions.js
+++ save questions.js
@@ -1,14 +1,261 @@
-let fst_year_fst_term_subjects = {
-    study_year_id: 1,
-    term_id: 1,
-    subjects: [
-        {
-            name: "العقيدة",
-            form_url: "https://docs.google.com/forms/d/1Gqqtw34JkaODRZHXKGmW_untLYWq8-lhU8_hM62PapA/edit?usp=forms_home&ths=true",
-            safwa_id: 1,
-        }
-    ]
-}
+let study_years = [
+    {
+        name: "fst_year_fst_term_subjects",
+        study_year_id: 1,
+        term_id: 1,
+        added: false,
+        subjects: [
+            {
+                safwa_name: "العقيدة الفصل الدراسي الأول للفرقة الأولى",
+                form_url: "https://docs.google.com/forms/d/1Gqqtw34JkaODRZHXKGmW_untLYWq8-lhU8_hM62PapA/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "الآداب الإسلامية الفصل الدراسي الأول للفرقة الأولى",
+                form_url: "https://docs.google.com/forms/d/1VSzLQMM4GC6PmANU67K7gWpzjxtfCIQEP6Gk01xvisU/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "الفقه الفصل الدراسي الأول للفرقة الأولى",
+                form_url: "https://docs.google.com/forms/d/1M9SCkKJ9SkKHDz_Nu5pU_33A-UA3S_r5nfAiZWWht5o/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "أصول الفقه الفصل الدراسي الأول للفرقة الأولى",
+                form_url: "https://docs.google.com/forms/d/1CQyRvqhOKSdvcAhoLghpfk-k47stJwn__FAD9cdGFEo/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "الحديث الفصل الدراسي الأول للفرقة الأولى",
+                form_url: "https://docs.google.com/forms/d/1NbvuZnFH_469v6z6lgsIiM713pA8ExLUAn9dQUPdZaU/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "النحو الفصل الدراسي الأول للفرقة الأولى",
+                form_url: "https://docs.google.com/forms/d/17S1Od3Dm7Peb22VeEk5_f3GaMN7gWkO9b7DMGrSjszI/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "التجويد الفصل الدراسي الأول للفرقة الأولى",
+                form_url: "https://docs.google.com/forms/d/1um5rdWounA-_hOBIGcpZbRFSR6xV-BKDoeslwt5ycG4/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "مصطلح الحديث الفصل الدراسي الأول للفرقة الأولى",
+                form_url: "https://docs.google.com/forms/d/11Eu-JOWmLaPgS_1Du_bMSG5nrnEyF7_Kp41auGeY-Jc/edit",
+                safwa_id: 1,
+            }
+        ]
+    },
+    {
+        name: "snd_year_fst_term_subjects",
+        study_year_id: 1,
+        term_id: 1,
+        added: false,
+        subjects: [
+            {
+                safwa_name: "التربية والسلوك الفصل الدراسي الأول للفرقة الثانية",
+                form_url: "https://docs.google.com/forms/d/1Er6wrc-AU4RiT0odFv5uOIN81bmTHsM92GbVurzG-EA/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "العقيدة الفصل الدراسي الأول للفرقة الثانية",
+                form_url: "https://docs.google.com/forms/d/1bACRIB8CVn4FgoOhIaD5rfZDgbOY4w9VljPE6rCED9M/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "تاريخ التشريع الفصل الدراسي الأول للفرقة الثانية",
+                form_url: "https://docs.google.com/forms/d/1llKx8WpCgLnt86JetB8AsxwayUZJB_8vWJyoPhTIqYE/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "الفقه المالكي الفصل الدراسي الأول للفرقة الثانية",
+                form_url: "https://docs.google.com/forms/d/1X3IuaGl36bD2UfmTE5ZWqMyaxrn7xZwomIuymUqQNvE/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "الفقه الشافعي الفصل الدراسي الأول للفرقة الثانية",
+                form_url: "https://docs.google.com/forms/d/1kV28rYlc6--yqHAcg0HvttSdrlA9hjJ-XBzrVM5BZMs/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "الفقه الحنبلي الفصل الدراسي الأول للفرقة الثانية",
+                form_url: "https://docs.google.com/forms/d/1qe5Wb-6m7RPExjLZ5MGDT_xquUkvXbSaZwDdJ-FN7go/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "الفقه الحنفي الفصل الدراسي الأول للفرقة الثانية",
+                form_url: "https://docs.google.com/forms/d/1cRDcyhvTTiwomIFZRkhaFITs23p0wIMynI7Y510KUBY/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "التفسير الفصل الدراسي الأول للفرقة الثانية",
+                form_url: "https://docs.google.com/forms/d/1eSFoghZN8ny4wMfKy7ot_gQY5vpHHJ-3Mx8s7HTexMs/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "البلاغة الفصل الدراسي الأول للفرقة الثانية",
+                form_url: "https://docs.google.com/forms/d/1c5x3Ywdj7Uwl_xqWZfgZeahxZ7FrGoZUFwx1rUgkzZY/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "السيرة الفصل الدراسي الأول للفرقة الثانية",
+                form_url: "https://docs.google.com/forms/d/1caqVW4y3EDc1solbYVpbuM6n0zAzml5HI-ykaDaJ5kY/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "علوم القرآن الفصل الدراسي الأول للفرقة الثانية",
+                form_url: "https://docs.google.com/forms/d/1z11EaYsGleWZhzMHGvx5P1RpOh0b7yRcAX7TGcGe7-Q/edit",
+                safwa_id: 1,
+            },
+        ]
+    },
+    {
+        name: "trd_year_fst_term_subjects",
+        study_year_id: 1,
+        term_id: 1,
+        added: false,
+        subjects: [
+            {
+                safwa_name: "التربية والسلوك الفصل الدراسي الأول للفرقة الثالثة",
+                form_url: "https://docs.google.com/forms/d/1lUrILYwD9ausG5fCihHuo7edn3hN6uhAJ65fgM5mnrI/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "العقيدة الفصل الدراسي الأول للفرقة الثالثة",
+                form_url: "https://docs.google.com/forms/d/1O-ekna5hu5gDV0ggUxrSHBzTX0H8L4wXEpRm_rKy9uE/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "الفقه العام الفصل الدراسي الأول للفرقة الثالثة",
+                form_url: "https://docs.google.com/forms/d/1asOLPedzF4bb8gbZoJAh52V25eN8gCuixcQ73fDXY_Y/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "الفقه الشافعي الفصل الدراسي الأول للفرقة الثالثة",
+                form_url: "https://docs.google.com/forms/d/1TND62NkyjrmSUG8wsuM659f9H8gf_AuK_bue1w-hLg4/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "الفقه المالكي الفصل الدراسي الأول للفرقة الثالثة",
+                form_url: "https://docs.google.com/forms/d/1Tq62bNR9T1m7o5ccyBoO1jROrJIH51PFaoLAxvXMFgw/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "الفقه الحنبلي الفصل الدراسي الأول للفرقة الثالثة",
+                form_url: "https://docs.google.com/forms/d/1l-9o6Sy9HBo3D_0mEwaxMTchBwWePwxd9ogYJes6jiQ/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "الفقه الحنفي الفصل الدراسي الأول للفرقة الثالثة",
+                form_url: "https://docs.google.com/forms/d/1Fv0a2OlwAW8jZ_dZkbRq6kCcF9bqDVLGEoOoO_qZ7o4/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "مصطلح الحديث الفصل الدراسي الأول للفرقة الثالثة",
+                form_url: "https://docs.google.com/forms/d/1x67gF8Q2eSMaPDNfvy1bu4oKXvzOSu_yqSjkY5XTgTo/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "القواعد الفقهية الفصل الدراسي الأول للفرقة الثالثة",
+                form_url: "https://docs.google.com/forms/d/1qG1EIonyqWquPx8rAlome_yuwWunx-q0FN7q0TmSsH8/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "التفسير الفصل الدراسي الأول للفرقة الثالثة",
+                form_url: "https://docs.google.com/forms/d/1jdc9cNsliJvteK43UwW3hrD-BDeBaZLWmRWWm-2qNmE/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "التجويد الفصل الدراسي الأول للفرقة الثالثة",
+                form_url: "https://docs.google.com/forms/d/1MXFuDj14iDgNmmK_g-WBZ7Mw2ImmTST41hOE_Vy0Qq8/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "أصول الفقه الفصل الدراسي الأول للفرقة الثالثة",
+                form_url: "https://docs.google.com/forms/d/1svqfJCoQZek3RSU32A2t9jyVyTxZ3BUsrWwi5i72CNg/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "الفرق والمذاهب الفصل الدراسي الأول للفرقة الثالثة",
+                form_url: "https://docs.google.com/forms/d/1Q6N0yy66nugZv1jF_NfOBqtp7QUZJuDra_dElcBsMUc/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "التاريخ الفصل الدراسي الأول للفرقة الثالثة",
+                form_url: "https://docs.google.com/forms/d/1tTINjSMd_Rc0vpwR_d5jQZICjoto8Xlx58k6l-dungo/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "اللغة العربية الفصل الدراسي الأول للفرقة الثالثة",
+                form_url: "https://docs.google.com/forms/d/1agul-yKRQNv5lWi43CXrfOg0H_5r1RNDiIgTYKWypnY/edit",
+                safwa_id: 1,
+            },
+        ]
+    },
+    {
+        name: "fth_year_fst_term_subjects",
+        study_year_id: 1,
+        term_id: 1,
+        added: false,
+        subjects: [
+            {
+                safwa_name: "التربية والسلوك الفصل الدراسي الأول للفرقة الرابعة",
+                form_url: "https://docs.google.com/forms/d/15tO312wAD-mOzWKGoibor_QLe0Pymv5_CCJ_aJrk-5Y/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "العقيدة الفصل الدراسي الأول للفرقة الرابعة",
+                form_url: "https://docs.google.com/forms/d/1NGkLEHZhsgzKReJVlhe2JHHAuMK6gZOx6mm3cqKyBfg/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "المحرر في الحديث (1) الفصل الدراسي الأول للفرقة الرابعة",
+                form_url: "https://docs.google.com/forms/d/1L2sLPnjkIsNFsqL6cXumMpsDXk3DX2n9vegEyFQpSIo/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "المحرر في الحديث (2) الفصل الدراسي الأول للفرقة الرابعة",
+                form_url: "https://docs.google.com/forms/d/1TdfA2QJp8pPtEPBhFzDPGLXrKkgaNLIPm00Zot7BtqY/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "القضايا المعاصرة (1) الفصل الدراسي الأول للفرقة الرابعة",
+                form_url: "https://docs.google.com/forms/d/1fcv8WPJR_GOcZMyFhrU2UL1WLYgryk32rRWpxAZY3ew/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "القضايا المعاصرة (2) الفصل الدراسي الأول للفرقة الرابعة",
+                form_url: "https://docs.google.com/forms/d/14H_KRfMRBmQjNtS3Sc5BUjf4ZUloTFEUtNmCYXFQz9Q/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "الأدب العربي الفصل الدراسي الأول للفرقة الرابعة",
+                form_url: "https://docs.google.com/forms/d/1Fzpcf4mfnZPR-AtuTAPf7NeIlcdRqPMM8Vh9SlsMDjI/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "الشبهات الفكرية الفصل الدراسي الأول للفرقة الرابعة",
+                form_url: "https://docs.google.com/forms/d/1M1jbT1lhR_vW90wzna0ktesICrIUFkPAC-_IXoOYx1Q/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "التاريخ الفصل الدراسي الأول للفرقة الرابعة",
+                form_url: "https://docs.google.com/forms/d/1BKhXTH79q2bClUeixgvP4cLrsOrKaV3y-VWBFGd2lts/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "مهارات البحث الفصل الدراسي الأول للفرقة الرابعة",
+                form_url: "https://docs.google.com/forms/d/1Y2Xzn3rGp_KuiDFIrNR7Xk7oWWzK54ZzB7w85HW7KqA/edit",
+                safwa_id: 1,
+            },
+            {
+                safwa_name: "المعاجم الفصل الدراسي الأول للفرقة الرابعة",
+                form_url: "https://docs.google.com/forms/d/1Nyb6Ki0ldlOdPnVsEnT9PFkWy01JjQe3AnQwN0HWYZk/edit",
+                safwa_id: 1,
+            }
+        ]
+    },]
 
 // variables for putting the questions and answers in the right position
 let question_row = 1
@@ -20,9 +267,13 @@ function main() {
 }
 
 function extractForms() {
-    fst_year_fst_term_subjects.subjects.forEach( subject => {
-        question_row = 1
-        extractFormQuestions(fst_year_fst_term_subjects, subject)
+    study_years.forEach(study_year => {
+        Logger.log(study_year.name)
+        study_year.subjects.forEach(subject => {
+            question_row = 1
+            extractFormQuestions(study_year, subject)
+        })
+        Logger.log("Done")
     })
 }
 
@@ -39,7 +290,7 @@ function insertHeader(sheet) {
     //path
     sheet.getRange(question_row, column++).setValue("path")
     //term
-    sheet.getRange(question_row, column++).setValue("TERM")
+    sheet.getRange(question_row, column++).setValue("term")
     //subject
     sheet.getRange(question_row, column++).setValue("subject")
     //lesson
@@ -51,12 +302,12 @@ function insertHeader(sheet) {
     //choices count
     sheet.getRange(question_row, column++).setValue("choices count")
     //coices
-    sheet.getRange(question_row, column++).setValue("choice1")
-    sheet.getRange(question_row, column++).setValue("choice2")
-    sheet.getRange(question_row, column++).setValue("choice3")
-    sheet.getRange(question_row, column++).setValue("choice4")
-    sheet.getRange(question_row, column++).setValue("choice5")
-    sheet.getRange(question_row, column++).setValue("choice6")
+    sheet.getRange(question_row, column++).setValue("select_1")
+    sheet.getRange(question_row, column++).setValue("select_2")
+    sheet.getRange(question_row, column++).setValue("select_3")
+    sheet.getRange(question_row, column++).setValue("select_4")
+    sheet.getRange(question_row, column++).setValue("select_5")
+    sheet.getRange(question_row, column++).setValue("select_6")
     //right answer
     sheet.getRange(question_row, column++).setValue("answer")
     question_row++
@@ -73,17 +324,17 @@ function extractFormQuestions(studyYearSubjects, subject) {
     form.getItems().forEach((item) => {
         switch (item.getType()) {
             case FormApp.ItemType.MULTIPLE_CHOICE:
-                insertMul(sheet, studyYearSubjects, item.asMultipleChoiceItem())
+                insertMul(sheet, studyYearSubjects, subject, item.asMultipleChoiceItem())
                 break
             case FormApp.ItemType.CHECKBOX:
-                insertCheckBoxQuestion(sheet, studyYearSubjects, item.asCheckboxItem())
+                insertCheckBoxQuestion(sheet, studyYearSubjects, subject, item.asCheckboxItem())
                 break
         }
     })
-    Logger.log(ssNewUrl)
+    Logger.log(subject.safwa_name + ": " + ssNewUrl)
 }
 
-function addRowBasicInfo(sheet, column, studyYearSubjects, title, points) {
+function addRowBasicInfo(sheet, column, studyYearSubjects, subject, title, points) {
     //title
     sheet.getRange(question_row, column++).setValue(title)
     //points
@@ -95,15 +346,15 @@ function addRowBasicInfo(sheet, column, studyYearSubjects, title, points) {
     //term
     sheet.getRange(question_row, column++).setValue(studyYearSubjects.term_id)
     //subject
-    sheet.getRange(question_row, column++).setValue(fst_year_fst_term_subjects.subjects[0].name)
+    sheet.getRange(question_row, column++).setValue(subject.safwa_name)
     //lesson
     sheet.getRange(question_row, column++).setValue("")
     return column;
 }
 
-function insertMul(sheet, studyYearSubjects, question) {
+function insertMul(sheet, studyYearSubjects, subject, question) {
     let column = 1
-    let type = "MULTIPLE_CHOICE"
+    let type = 1
     let points = question.getPoints()
     if (points === 0) {
         return
@@ -127,12 +378,12 @@ function insertMul(sheet, studyYearSubjects, question) {
     title = formatQuestionTitle(title)
     let choices = question.getChoices()
 
-    column = addRowBasicInfo(sheet, column, studyYearSubjects, title, points);
+    column = addRowBasicInfo(sheet, column, studyYearSubjects, subject, title, points);
 
     if (choices.length === 2) {
         if ((choices[0].getValue().includes("صح") && choices[1].getValue().includes("خطأ")) ||
             (choices[1].getValue().includes("صح") && choices[0].getValue().includes("خطأ"))) {
-            type = "binary"
+            type = 2
             //question type
             sheet.getRange(question_row, column++).setValue(type)
             //description
@@ -155,7 +406,7 @@ function insertMul(sheet, studyYearSubjects, question) {
     question_row++
 }
 
-function insertCheckBoxQuestion(sheet, studyYearSubjects, question) {
+function insertCheckBoxQuestion(sheet, studyYearSubjects, subject, question) {
     let coloumn = 1
     let title = formatQuestionTitle(question.getTitle())
     let points = question.getPoints()
@@ -163,10 +414,10 @@ function insertCheckBoxQuestion(sheet, studyYearSubjects, question) {
         return
     }
 
-    let type = "CHECKBOX"
+    let type = 4
     let choices = question.getChoices()
 
-    coloumn = addRowBasicInfo(sheet, coloumn, studyYearSubjects, title, points);
+    coloumn = addRowBasicInfo(sheet, coloumn, studyYearSubjects, subject, title, points);
     //question type
     sheet.getRange(question_row, coloumn++).setValue(type)
     //description
@@ -183,9 +434,9 @@ function addBinaryChoice(sheet, choices) {
     choices.forEach(function (choice) {
         if (choice.isCorrectAnswer()) {
             if (choice.getValue().includes("صح")) {
-                sheet.getRange(question_row, right_answer_index).setValue("صح")
+                sheet.getRange(question_row, right_answer_index).setValue(1)
             } else {
-                sheet.getRange(question_row, right_answer_index).setValue("خطأ")
+                sheet.getRange(question_row, right_answer_index).setValue(0)
             }
         }
     })
@@ -196,10 +447,14 @@ function addCheckBoxChoice(sheet, coloumn, choices) {
     choices.forEach(function (choice, index) {
         sheet.getRange(question_row, coloumn++).setValue(choice.getValue())
         if (choice.isCorrectAnswer()) {
-            let correct_answer = sheet.getRange(question_row, right_answer_index).getValue()
-            sheet.getRange(question_row, right_answer_index).setValue(correct_answer + (index + 1) + ",")
+            let correct_answers = sheet.getRange(question_row, right_answer_index).getValue()
+            sheet.getRange(question_row, right_answer_index).setValue(correct_answers + (index + 1) + ",")
         }
     })
+    let correct_answers = sheet.getRange(question_row, right_answer_index).getValue()
+    if (correct_answers.endsWith(",")) {
+        sheet.getRange(question_row, right_answer_index).setValue(correct_answers.substring(0, correct_answers.length - 1))
+    }
 }
 
 function addMultipleChoices(sheet, column, choices) {
