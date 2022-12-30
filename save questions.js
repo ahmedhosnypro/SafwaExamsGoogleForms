d09f69c46958f19bd2d7e796d7fb40d25c0e2f4f
Ahmed Hosny <ahhosnyas@gmail.com>
2022-12-31 00:02:08 +0200

    minor update

--- save questions.js
+++ save questions.js
@@ -7,44 +7,28 @@ let study_years = [
         subjects: [
             {
                 safwa_name: "العقيدة الفصل الدراسي الأول للفرقة الأولى",
-                form_url: "https://docs.google.com/forms/d/1Gqqtw34JkaODRZHXKGmW_untLYWq8-lhU8_hM62PapA/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1Gqqtw34JkaODRZHXKGmW_untLYWq8-lhU8_hM62PapA/edit" },
             {
                 safwa_name: "الآداب الإسلامية الفصل الدراسي الأول للفرقة الأولى",
-                form_url: "https://docs.google.com/forms/d/1VSzLQMM4GC6PmANU67K7gWpzjxtfCIQEP6Gk01xvisU/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1VSzLQMM4GC6PmANU67K7gWpzjxtfCIQEP6Gk01xvisU/edit" },
             {
                 safwa_name: "الفقه الفصل الدراسي الأول للفرقة الأولى",
-                form_url: "https://docs.google.com/forms/d/1M9SCkKJ9SkKHDz_Nu5pU_33A-UA3S_r5nfAiZWWht5o/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1M9SCkKJ9SkKHDz_Nu5pU_33A-UA3S_r5nfAiZWWht5o/edit" },
             {
                 safwa_name: "أصول الفقه الفصل الدراسي الأول للفرقة الأولى",
-                form_url: "https://docs.google.com/forms/d/1CQyRvqhOKSdvcAhoLghpfk-k47stJwn__FAD9cdGFEo/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1CQyRvqhOKSdvcAhoLghpfk-k47stJwn__FAD9cdGFEo/edit" },
             {
                 safwa_name: "الحديث الفصل الدراسي الأول للفرقة الأولى",
-                form_url: "https://docs.google.com/forms/d/1NbvuZnFH_469v6z6lgsIiM713pA8ExLUAn9dQUPdZaU/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1NbvuZnFH_469v6z6lgsIiM713pA8ExLUAn9dQUPdZaU/edit" },
             {
                 safwa_name: "النحو الفصل الدراسي الأول للفرقة الأولى",
-                form_url: "https://docs.google.com/forms/d/17S1Od3Dm7Peb22VeEk5_f3GaMN7gWkO9b7DMGrSjszI/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/17S1Od3Dm7Peb22VeEk5_f3GaMN7gWkO9b7DMGrSjszI/edit" },
             {
                 safwa_name: "التجويد الفصل الدراسي الأول للفرقة الأولى",
-                form_url: "https://docs.google.com/forms/d/1um5rdWounA-_hOBIGcpZbRFSR6xV-BKDoeslwt5ycG4/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1um5rdWounA-_hOBIGcpZbRFSR6xV-BKDoeslwt5ycG4/edit" },
             {
                 safwa_name: "مصطلح الحديث الفصل الدراسي الأول للفرقة الأولى",
-                form_url: "https://docs.google.com/forms/d/11Eu-JOWmLaPgS_1Du_bMSG5nrnEyF7_Kp41auGeY-Jc/edit",
-                safwa_id: 1,
-            }
+                form_url: "https://docs.google.com/forms/d/11Eu-JOWmLaPgS_1Du_bMSG5nrnEyF7_Kp41auGeY-Jc/edit" }
         ]
     },
     {
@@ -55,59 +39,37 @@ let study_years = [
         subjects: [
             {
                 safwa_name: "التربية والسلوك الفصل الدراسي الأول للفرقة الثانية",
-                form_url: "https://docs.google.com/forms/d/1Er6wrc-AU4RiT0odFv5uOIN81bmTHsM92GbVurzG-EA/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1Er6wrc-AU4RiT0odFv5uOIN81bmTHsM92GbVurzG-EA/edit" },
             {
                 safwa_name: "العقيدة الفصل الدراسي الأول للفرقة الثانية",
-                form_url: "https://docs.google.com/forms/d/1bACRIB8CVn4FgoOhIaD5rfZDgbOY4w9VljPE6rCED9M/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1bACRIB8CVn4FgoOhIaD5rfZDgbOY4w9VljPE6rCED9M/edit" },
             {
                 safwa_name: "تاريخ التشريع الفصل الدراسي الأول للفرقة الثانية",
-                form_url: "https://docs.google.com/forms/d/1llKx8WpCgLnt86JetB8AsxwayUZJB_8vWJyoPhTIqYE/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1llKx8WpCgLnt86JetB8AsxwayUZJB_8vWJyoPhTIqYE/edit" },
             {
                 safwa_name: "الفقه المالكي الفصل الدراسي الأول للفرقة الثانية",
-                form_url: "https://docs.google.com/forms/d/1X3IuaGl36bD2UfmTE5ZWqMyaxrn7xZwomIuymUqQNvE/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1X3IuaGl36bD2UfmTE5ZWqMyaxrn7xZwomIuymUqQNvE/edit" },
             {
                 safwa_name: "الفقه الشافعي الفصل الدراسي الأول للفرقة الثانية",
-                form_url: "https://docs.google.com/forms/d/1kV28rYlc6--yqHAcg0HvttSdrlA9hjJ-XBzrVM5BZMs/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1kV28rYlc6--yqHAcg0HvttSdrlA9hjJ-XBzrVM5BZMs/edit" },
             {
                 safwa_name: "الفقه الحنبلي الفصل الدراسي الأول للفرقة الثانية",
-                form_url: "https://docs.google.com/forms/d/1qe5Wb-6m7RPExjLZ5MGDT_xquUkvXbSaZwDdJ-FN7go/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1qe5Wb-6m7RPExjLZ5MGDT_xquUkvXbSaZwDdJ-FN7go/edit" },
             {
                 safwa_name: "الفقه الحنفي الفصل الدراسي الأول للفرقة الثانية",
-                form_url: "https://docs.google.com/forms/d/1cRDcyhvTTiwomIFZRkhaFITs23p0wIMynI7Y510KUBY/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1cRDcyhvTTiwomIFZRkhaFITs23p0wIMynI7Y510KUBY/edit" },
             {
                 safwa_name: "التفسير الفصل الدراسي الأول للفرقة الثانية",
-                form_url: "https://docs.google.com/forms/d/1eSFoghZN8ny4wMfKy7ot_gQY5vpHHJ-3Mx8s7HTexMs/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1eSFoghZN8ny4wMfKy7ot_gQY5vpHHJ-3Mx8s7HTexMs/edit" },
             {
                 safwa_name: "البلاغة الفصل الدراسي الأول للفرقة الثانية",
-                form_url: "https://docs.google.com/forms/d/1c5x3Ywdj7Uwl_xqWZfgZeahxZ7FrGoZUFwx1rUgkzZY/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1c5x3Ywdj7Uwl_xqWZfgZeahxZ7FrGoZUFwx1rUgkzZY/edit" },
             {
                 safwa_name: "السيرة الفصل الدراسي الأول للفرقة الثانية",
-                form_url: "https://docs.google.com/forms/d/1caqVW4y3EDc1solbYVpbuM6n0zAzml5HI-ykaDaJ5kY/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1caqVW4y3EDc1solbYVpbuM6n0zAzml5HI-ykaDaJ5kY/edit" },
             {
                 safwa_name: "علوم القرآن الفصل الدراسي الأول للفرقة الثانية",
-                form_url: "https://docs.google.com/forms/d/1z11EaYsGleWZhzMHGvx5P1RpOh0b7yRcAX7TGcGe7-Q/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1z11EaYsGleWZhzMHGvx5P1RpOh0b7yRcAX7TGcGe7-Q/edit" },
         ]
     },
     {
@@ -118,79 +80,49 @@ let study_years = [
         subjects: [
             {
                 safwa_name: "التربية والسلوك الفصل الدراسي الأول للفرقة الثالثة",
-                form_url: "https://docs.google.com/forms/d/1lUrILYwD9ausG5fCihHuo7edn3hN6uhAJ65fgM5mnrI/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1lUrILYwD9ausG5fCihHuo7edn3hN6uhAJ65fgM5mnrI/edit" },
             {
                 safwa_name: "العقيدة الفصل الدراسي الأول للفرقة الثالثة",
-                form_url: "https://docs.google.com/forms/d/1O-ekna5hu5gDV0ggUxrSHBzTX0H8L4wXEpRm_rKy9uE/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1O-ekna5hu5gDV0ggUxrSHBzTX0H8L4wXEpRm_rKy9uE/edit" },
             {
                 safwa_name: "الفقه العام الفصل الدراسي الأول للفرقة الثالثة",
-                form_url: "https://docs.google.com/forms/d/1asOLPedzF4bb8gbZoJAh52V25eN8gCuixcQ73fDXY_Y/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1asOLPedzF4bb8gbZoJAh52V25eN8gCuixcQ73fDXY_Y/edit" },
             {
                 safwa_name: "الفقه الشافعي الفصل الدراسي الأول للفرقة الثالثة",
-                form_url: "https://docs.google.com/forms/d/1TND62NkyjrmSUG8wsuM659f9H8gf_AuK_bue1w-hLg4/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1TND62NkyjrmSUG8wsuM659f9H8gf_AuK_bue1w-hLg4/edit" },
             {
                 safwa_name: "الفقه المالكي الفصل الدراسي الأول للفرقة الثالثة",
-                form_url: "https://docs.google.com/forms/d/1Tq62bNR9T1m7o5ccyBoO1jROrJIH51PFaoLAxvXMFgw/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1Tq62bNR9T1m7o5ccyBoO1jROrJIH51PFaoLAxvXMFgw/edit" },
             {
                 safwa_name: "الفقه الحنبلي الفصل الدراسي الأول للفرقة الثالثة",
-                form_url: "https://docs.google.com/forms/d/1l-9o6Sy9HBo3D_0mEwaxMTchBwWePwxd9ogYJes6jiQ/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1l-9o6Sy9HBo3D_0mEwaxMTchBwWePwxd9ogYJes6jiQ/edit" },
             {
                 safwa_name: "الفقه الحنفي الفصل الدراسي الأول للفرقة الثالثة",
-                form_url: "https://docs.google.com/forms/d/1Fv0a2OlwAW8jZ_dZkbRq6kCcF9bqDVLGEoOoO_qZ7o4/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1Fv0a2OlwAW8jZ_dZkbRq6kCcF9bqDVLGEoOoO_qZ7o4/edit" },
             {
                 safwa_name: "مصطلح الحديث الفصل الدراسي الأول للفرقة الثالثة",
-                form_url: "https://docs.google.com/forms/d/1x67gF8Q2eSMaPDNfvy1bu4oKXvzOSu_yqSjkY5XTgTo/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1x67gF8Q2eSMaPDNfvy1bu4oKXvzOSu_yqSjkY5XTgTo/edit" },
             {
                 safwa_name: "القواعد الفقهية الفصل الدراسي الأول للفرقة الثالثة",
-                form_url: "https://docs.google.com/forms/d/1qG1EIonyqWquPx8rAlome_yuwWunx-q0FN7q0TmSsH8/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1qG1EIonyqWquPx8rAlome_yuwWunx-q0FN7q0TmSsH8/edit" },
             {
                 safwa_name: "التفسير الفصل الدراسي الأول للفرقة الثالثة",
-                form_url: "https://docs.google.com/forms/d/1jdc9cNsliJvteK43UwW3hrD-BDeBaZLWmRWWm-2qNmE/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1jdc9cNsliJvteK43UwW3hrD-BDeBaZLWmRWWm-2qNmE/edit" },
             {
                 safwa_name: "التجويد الفصل الدراسي الأول للفرقة الثالثة",
-                form_url: "https://docs.google.com/forms/d/1MXFuDj14iDgNmmK_g-WBZ7Mw2ImmTST41hOE_Vy0Qq8/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1MXFuDj14iDgNmmK_g-WBZ7Mw2ImmTST41hOE_Vy0Qq8/edit" },
             {
                 safwa_name: "أصول الفقه الفصل الدراسي الأول للفرقة الثالثة",
-                form_url: "https://docs.google.com/forms/d/1svqfJCoQZek3RSU32A2t9jyVyTxZ3BUsrWwi5i72CNg/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1svqfJCoQZek3RSU32A2t9jyVyTxZ3BUsrWwi5i72CNg/edit" },
             {
                 safwa_name: "الفرق والمذاهب الفصل الدراسي الأول للفرقة الثالثة",
-                form_url: "https://docs.google.com/forms/d/1Q6N0yy66nugZv1jF_NfOBqtp7QUZJuDra_dElcBsMUc/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1Q6N0yy66nugZv1jF_NfOBqtp7QUZJuDra_dElcBsMUc/edit" },
             {
                 safwa_name: "التاريخ الفصل الدراسي الأول للفرقة الثالثة",
-                form_url: "https://docs.google.com/forms/d/1tTINjSMd_Rc0vpwR_d5jQZICjoto8Xlx58k6l-dungo/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1tTINjSMd_Rc0vpwR_d5jQZICjoto8Xlx58k6l-dungo/edit" },
             {
                 safwa_name: "اللغة العربية الفصل الدراسي الأول للفرقة الثالثة",
-                form_url: "https://docs.google.com/forms/d/1agul-yKRQNv5lWi43CXrfOg0H_5r1RNDiIgTYKWypnY/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1agul-yKRQNv5lWi43CXrfOg0H_5r1RNDiIgTYKWypnY/edit" },
         ]
     },
     {
@@ -201,59 +133,37 @@ let study_years = [
         subjects: [
             {
                 safwa_name: "التربية والسلوك الفصل الدراسي الأول للفرقة الرابعة",
-                form_url: "https://docs.google.com/forms/d/15tO312wAD-mOzWKGoibor_QLe0Pymv5_CCJ_aJrk-5Y/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/15tO312wAD-mOzWKGoibor_QLe0Pymv5_CCJ_aJrk-5Y/edit" },
             {
                 safwa_name: "العقيدة الفصل الدراسي الأول للفرقة الرابعة",
-                form_url: "https://docs.google.com/forms/d/1NGkLEHZhsgzKReJVlhe2JHHAuMK6gZOx6mm3cqKyBfg/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1NGkLEHZhsgzKReJVlhe2JHHAuMK6gZOx6mm3cqKyBfg/edit" },
             {
                 safwa_name: "المحرر في الحديث (1) الفصل الدراسي الأول للفرقة الرابعة",
-                form_url: "https://docs.google.com/forms/d/1L2sLPnjkIsNFsqL6cXumMpsDXk3DX2n9vegEyFQpSIo/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1L2sLPnjkIsNFsqL6cXumMpsDXk3DX2n9vegEyFQpSIo/edit" },
             {
                 safwa_name: "المحرر في الحديث (2) الفصل الدراسي الأول للفرقة الرابعة",
-                form_url: "https://docs.google.com/forms/d/1TdfA2QJp8pPtEPBhFzDPGLXrKkgaNLIPm00Zot7BtqY/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1TdfA2QJp8pPtEPBhFzDPGLXrKkgaNLIPm00Zot7BtqY/edit" },
             {
                 safwa_name: "القضايا المعاصرة (1) الفصل الدراسي الأول للفرقة الرابعة",
-                form_url: "https://docs.google.com/forms/d/1fcv8WPJR_GOcZMyFhrU2UL1WLYgryk32rRWpxAZY3ew/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1fcv8WPJR_GOcZMyFhrU2UL1WLYgryk32rRWpxAZY3ew/edit" },
             {
                 safwa_name: "القضايا المعاصرة (2) الفصل الدراسي الأول للفرقة الرابعة",
-                form_url: "https://docs.google.com/forms/d/14H_KRfMRBmQjNtS3Sc5BUjf4ZUloTFEUtNmCYXFQz9Q/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/14H_KRfMRBmQjNtS3Sc5BUjf4ZUloTFEUtNmCYXFQz9Q/edit" },
             {
                 safwa_name: "الأدب العربي الفصل الدراسي الأول للفرقة الرابعة",
-                form_url: "https://docs.google.com/forms/d/1Fzpcf4mfnZPR-AtuTAPf7NeIlcdRqPMM8Vh9SlsMDjI/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1Fzpcf4mfnZPR-AtuTAPf7NeIlcdRqPMM8Vh9SlsMDjI/edit" },
             {
                 safwa_name: "الشبهات الفكرية الفصل الدراسي الأول للفرقة الرابعة",
-                form_url: "https://docs.google.com/forms/d/1M1jbT1lhR_vW90wzna0ktesICrIUFkPAC-_IXoOYx1Q/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1M1jbT1lhR_vW90wzna0ktesICrIUFkPAC-_IXoOYx1Q/edit" },
             {
                 safwa_name: "التاريخ الفصل الدراسي الأول للفرقة الرابعة",
-                form_url: "https://docs.google.com/forms/d/1BKhXTH79q2bClUeixgvP4cLrsOrKaV3y-VWBFGd2lts/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1BKhXTH79q2bClUeixgvP4cLrsOrKaV3y-VWBFGd2lts/edit" },
             {
                 safwa_name: "مهارات البحث الفصل الدراسي الأول للفرقة الرابعة",
-                form_url: "https://docs.google.com/forms/d/1Y2Xzn3rGp_KuiDFIrNR7Xk7oWWzK54ZzB7w85HW7KqA/edit",
-                safwa_id: 1,
-            },
+                form_url: "https://docs.google.com/forms/d/1Y2Xzn3rGp_KuiDFIrNR7Xk7oWWzK54ZzB7w85HW7KqA/edit" },
             {
                 safwa_name: "المعاجم الفصل الدراسي الأول للفرقة الرابعة",
-                form_url: "https://docs.google.com/forms/d/1Nyb6Ki0ldlOdPnVsEnT9PFkWy01JjQe3AnQwN0HWYZk/edit",
-                safwa_id: 1,
-            }
+                form_url: "https://docs.google.com/forms/d/1Nyb6Ki0ldlOdPnVsEnT9PFkWy01JjQe3AnQwN0HWYZk/edit" }
         ]
     },]
 
